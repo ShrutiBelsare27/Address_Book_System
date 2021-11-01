2483e5c09b1d627784718523e217c9a67415eb9c
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-11-01 13:13:24 +0530

    Added: Address book

---
+++ Day10AddressBook/AddressBookSystem/AddressBookSystem/AddressBook.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class AddressBook
+{
+    List<Address> addresses;
+
+    public AddressBook()
+    {
+        addresses = new List<Address>();
+    }
+
+    public bool add(string name, string address, string city, string state, string zip, string phoneNumber, string email)
+    {
+        Address addr = new Address(name, address, city, state, zip, phoneNumber, email);
+        Address result = find(name);
+        //if (book.add(name, address, city, state, zip, phoneNumber, email))
+
+        if (result == null)
+        {
+            addresses.Add(addr);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool remove(string name)
+    {
+        Address addr = find(name);
+
+        if (addr != null)
+        {
+            addresses.Remove(addr);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void list(Action<Address> action)
+    {
+        addresses.ForEach(action);
+    }
+
+    public bool isEmpty()
+    {
+        return (addresses.Count == 0);
+    }
+
+    public Address find(string name)
+    {
+        Address addr = addresses.Find(
+          delegate (Address a) {
+              return a.name == name;
+          }
+        );
+        return addr;
+    }
+
+
+}
---
+++ Day10AddressBook/AddressBookSystem/AddressBookSystem/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+
+class AddressPrompt
+{
+    AddressBook book;
+
+    public AddressPrompt()
+    {
+        book = new AddressBook();
+    }
+
+    static void Main(string[] args)
+    {
+        string selection = "";
+        AddressPrompt prompt = new AddressPrompt();
+
+        prompt.displayMenu();
+        while (!selection.ToUpper().Equals("Q"))
+        {
+            Console.WriteLine("Selection: ");
+            selection = Console.ReadLine();
+            prompt.performAction(selection);
+        }
+    }
+
+    void displayMenu()
+    {
+    
+        Console.WriteLine("Main Menu");
+        Console.WriteLine("A - Add an Address");
+        Console.WriteLine("E - Edit an Address");
+        Console.WriteLine("D - Delete an Address");
+        Console.WriteLine("L - List All Addresses");
+        Console.WriteLine("Q - Quit");
+    }
+
+    void performAction(string selection)
+    {
+        string name = "";
+        string address = "";
+        string city = "";
+        string state = "";
+        string zip = "";
+        string phoneNumber = "";
+        string email = "";
+
+        switch (selection)
+        {
+            case "A":
+                Console.WriteLine("Enter Name: ");
+                name = Console.ReadLine();
+                Console.WriteLine("Enter Address: ");
+                address = Console.ReadLine();
+                Console.WriteLine("Enter City: ");
+                city = Console.ReadLine();
+                Console.WriteLine("Enter State: ");
+                state = Console.ReadLine();
+                Console.WriteLine("Enter Zip: ");
+                zip = Console.ReadLine();
+                Console.WriteLine("Enter Phone Number: ");
+                phoneNumber = Console.ReadLine();
+                Console.WriteLine("Enter Email: ");
+                email = Console.ReadLine();
+
+
+                if (book.add(name, address, city, state, zip, phoneNumber, email))
+                {
+                    Console.WriteLine("Address successfully added!");
+                }
+                else
+                {
+                    Console.WriteLine("An address is already on file for {0}.", name);
+                }
+                break;
+            case "D":
+                Console.WriteLine("Enter Name to Delete: ");
+                name = Console.ReadLine();
+                if (book.remove(name))
+                {
+                    Console.WriteLine("Address successfully removed");
+                }
+                else
+                {
+                    Console.WriteLine("Address for {0} could not be found.", name);
+                }
+                break;
+            case "L":
+                if (book.isEmpty())
+                {
+                    Console.WriteLine("There are no entries.");
+                }
+                else
+                {
+                    //Address a;
+                    Console.WriteLine("Addresses:");
+                    book.list(
+                      delegate (Address a) {
+                          Console.WriteLine("{0} - {1}", a.name, a.address);
+                          //   Console.WriteLine("{2}", a.city);
+                      }
+                    );
+                }
+                break;
+            /*
+                      Console.WriteLine("{0}|{1}", a.name, a.address, a.city, a.state, a.zip, a.phoneNumber, a.email);
+                */
+            case "E":
+                Console.WriteLine("Enter Name to Edit: ");
+                name = Console.ReadLine();
+                Address addr = book.find(name);
+                if (addr == null)
+                {
+                    Console.WriteLine("Address for {0} count not be found.", name);
+                }
+                else
+                {
+                    Console.WriteLine("Enter new Address: ");
+                    addr.address = Console.ReadLine();
+                    Console.WriteLine("Enter City: ");
+                    city = Console.ReadLine();
+                    Console.WriteLine("Enter State: ");
+                    state = Console.ReadLine();
+                    Console.WriteLine("Enter Zip: ");
+                    zip = Console.ReadLine();
+                    Console.WriteLine("Enter Phone Number: ");
+                    phoneNumber = Console.ReadLine();
+                    Console.WriteLine("Enter Email: ");
+                    email = Console.ReadLine();
+                    Console.WriteLine("Address updated for {0}", name);
+                }
+                break;
+                /*
+
+                    Console.WriteLine("Address updated for {0}|{1}", name, address, city, state, zip, phoneNumber, email);
+                  
+                }
+                break;*/
+        }
+    }
+}
